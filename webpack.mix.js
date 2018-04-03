32bef5972457e64db3ccb3cdd66d66d40ba65fd4
Ashok Gelal <ashokgelal@gmail.com>
2018-04-02 22:26:34 -0600

    Update npm packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+let mix = require('laravel-mix')
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +12,10 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+  .sass('resources/assets/sass/app.scss', 'public/css')
+  .browserSync({
+    open: 'external',
+    host: 'boise.townhouse.test',
+    proxy: 'boise.townhouse.test',
+    notify: false
+  }).disableNotifications()
